ea09679c9ad8448184806ec8b7da23a7d5280ec8
Sneax421 <curilov.german@web.de>
2025-03-25 11:58:53 +0100

    Complete navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,11 @@
 import Navitem from "./Navitem.jsx";
 import {navItems} from "../utils/constants.js";
 
-const Navigation = () => {
+const Navigation = ({changePage}) => {
     return (
         <nav className="fixed-top mt-2 ms-5">
             <ul className="nav">
-                {navItems.map(item =>  <Navitem itemTitle={item} key={item}/>)}
-
+                {navItems.map(item =>  <Navitem changePage={changePage} itemTitle={item} key={item}/>)}
             </ul>
         </nav>
     );
