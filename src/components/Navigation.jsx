8a2f2c3945b418a4d77088fa77defb2fd69d1cde
Sneax421 <curilov.german@web.de>
2025-03-26 18:34:16 +0100

    Add context

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 import Navitem from "./Navitem.jsx";
 import {navItems} from "../utils/constants.js";
 
-const Navigation = ({changePage}) => {
+const Navigation = () => {
     return (
         <nav className="fixed-top mt-2 ms-5">
             <ul className="nav">
-                {navItems.map(item =>  <Navitem changePage={changePage} itemTitle={item} key={item}/>)}
+                {navItems.map(item =>  <Navitem itemTitle={item} key={item}/>)}
             </ul>
         </nav>
     );
--- src/components/Navitem.jsx
+++ src/components/Navitem.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {StarWarsContext} from "../utils/context.js";
 
-const Navitem = ({itemTitle, changePage}) => {
+const Navitem = ({itemTitle}) => {
+    const {changePage} = useContext(StarWarsContext);
     return (
         <li onClick={() => changePage(itemTitle)} className="nav-item btn btn-danger mx-1">{itemTitle}</li>
     );
