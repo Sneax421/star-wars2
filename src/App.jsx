d6ade68a820f8b23fcbb22d28c679dd492a5986c
Sneax421 <curilov.german@web.de>
2025-03-26 18:27:43 +0100

    Add context

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
 import {useState} from "react";
 import {navItems} from "./utils/constants.js";
+import {StarWarsContext} from "./utils/context.js";
 
 function App() {
     const [page, setPage] = useState(navItems[0]);
     return (
         <div className={'container-fluid'}>
-            <Header changePage={setPage} />
-            <Main page={page}/>
-            <Footer/>
+            <StarWarsContext.Provider value={{
+                page,
+                changePage: setPage
+            }}>
+                <Header changePage={setPage}/>
+                <Main page={page}/>
+                <Footer/>
+            </StarWarsContext.Provider>
         </div>
     )
 }
 
 export default App
+
+
