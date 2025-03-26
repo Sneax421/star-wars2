a7be9ab4b1793138780e3decebc144c8c2b6dd47
Sneax421 <curilov.german@web.de>
2025-03-26 09:39:54 +0100

    About me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,39 +1,93 @@
-import {useEffect, useState} from 'react';
+// import {useEffect, useState} from 'react';
+// import {base_url} from "../utils/constants.js";
+// import main from "../assets/main.jpg";
+//
+// const AboutMe = () => {
+//     const [hero, setHero] = useState('Loading...');
+//
+//     useEffect(() => {
+//         fetch(`${base_url}/v1/peoples/1`)
+//             .then(res => res.json())
+//             .then(data => {
+//                 setHero({
+//                     name: data.name,
+//                     gender: data.gender,
+//                     height: data.height,
+//                     eye_color: data.eye_color,
+//                     mass: data.mass,
+//                     birth_year: data.birth_year,
+//
+//                 });
+//             })
+//             .catch(() => setHero('Error'))
+//     })
+//
+//
+//     return (
+//         <div className={'farGalaxy'}>
+//             <img className="float-start w-25 me-3" src={main} alt="Hero"/>
+//             <p>Name: {hero.name}</p>
+//             <p>Gender: {hero.gender}</p>
+//             <p>Height: {hero.height}</p>
+//             <p>Eye color: {hero.eye_color}</p>
+//             <p>Mass: {hero.mass}</p>
+//             <p>Birth year: {hero.birth_year}</p>
+//         </div>
+//     );
+// };
+//
+// export default AboutMe;
+
+
+
 import {base_url} from "../utils/constants.js";
-import main from "../assets/main.jpg";
+import {useEffect, useState} from "react";
 
 const AboutMe = () => {
-    const [hero, setHero] = useState('Loading...');
-
+    const [hero, setHero] = useState();
     useEffect(() => {
         fetch(`${base_url}/v1/peoples/1`)
-            .then(res => res.json())
+            .then(response => response.json())
             .then(data => {
-                setHero({
+                const info = {
                     name: data.name,
                     gender: data.gender,
-                    height: data.height,
-                    eye_color: data.eye_color,
-                    mass: data.mass,
                     birth_year: data.birth_year,
-
-                });
+                    height: data.height,
+                    mass: data.mass,
+                    hair_color: data.hair_color,
+                    skin_color: data.skin_color,
+                    eye_color: data.eye_color
+                }
+                setHero(info);
             })
-            .catch(() => setHero('Error'))
-    })
-
+    }, [])
 
     return (
-        <div className={'farGalaxy'}>
-            <img className="float-start w-25 me-3" src={main} alt="Hero"/>
-            <p>Name: {hero.name}</p>
-            <p>Gender: {hero.gender}</p>
-            <p>Height: {hero.height}</p>
-            <p>Eye color: {hero.eye_color}</p>
-            <p>Mass: {hero.mass}</p>
-            <p>Birth year: {hero.birth_year}</p>
-        </div>
+        <>
+            {(!!hero) &&
+                <div className='fs-2 lh-lg text-justify ms-5'>
+                    <p><span className='display-3'>name:</span> {hero.name}</p>
+                    <p><span className='display-3'>gender:</span> {hero.gender}</p>
+                    <p><span className='display-3'>birth year:</span> {hero.birth_year}</p>
+                    <p><span className='display-3'>height:</span> {hero.height}</p>
+                    <p><span className='display-3'>mass:</span> {hero.mass}</p>
+                    <p><span className='display-3'>hair color:</span> {hero.hair_color}</p>
+                    <p><span className='display-3'>skin color:</span> {hero.skin_color}</p>
+                    <p><span className='display-3'>eye color:</span> {hero.eye_color}</p>
+                </div>
+            }
+        </>
     );
 };
 
 export default AboutMe;
+
+
+
+
+
+
+
+
+
