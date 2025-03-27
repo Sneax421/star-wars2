8d0c729f1a4ec95ed29c6553508aab80de595cc3
Sneax421 <curilov.german@web.de>
2025-03-27 12:50:35 +0100

    local storage

--- src/components/FarGalaxy.jsx
+++ src/components/FarGalaxy.jsx
@@ -5,17 +5,26 @@ const FarGalaxy = () => {
     const [openingCrawl, setOpeningCrawl] = useState('Loading...');
 
     useEffect(() => {
-        const episode = Math.floor(Math.random() * 6 + 1);
-        fetch(`${base_url}/v1/films/${episode}`)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error('Failed to fetch films!');
-                }
-                return res.json();
-            })
-            .then(data => setOpeningCrawl(data.opening_crawl))
-            .catch(e => setOpeningCrawl(e.message));
+        const opening_crawl = sessionStorage.getItem("opening_crawl");
+        if (opening_crawl) {
+            setOpeningCrawl(opening_crawl);
+        }else {
+            const episode = Math.floor(Math.random() * 6 + 1);
+            fetch(`${base_url}/v1/films/${episode}`)
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error('Failed to fetch films!');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setOpeningCrawl(data.opening_crawl);
+                    sessionStorage.setItem("opening_crawl", data.opening_crawl);
+                })
+                .catch(e => setOpeningCrawl(e.message));
+        }
     }, [])
+
 
     return (
         <p className="farGalaxy">
